1b253848c6fa4c6db84cc4f13e5d34dc54d0fe02
Andrew Head <head.andrewm@gmail.com>
2021-04-28 10:50:26 -0700

    Add setup.py to enable installation using pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="texcompile",
+    version="0.0.1",
+    packages=["texcompile"],
+    license="Apache License 2.0",
+    long_description=open("README.md").read(),
+    url="https://github.com/andrewhead/texcompile",
+    install_requires=["requests>=2.0.0,<3.0.0",],
+)
