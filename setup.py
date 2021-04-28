a717334f939a395de9ea6c6e9a94ffde0323b1ed
Andrew Head <head.andrewm@gmail.com>
2021-04-28 12:58:13 -0700

    Client: Updated 'setup.py'

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ from distutils.core import setup
 setup(
     name="texcompile",
     version="0.0.1",
-    packages=["."],
+    packages=["client"],
     license="Apache License 2.0",
     long_description=open("README.md").read(),
     url="https://github.com/andrewhead/texcompile",
-    install_requires=["requests>=2.0.0,<3.0.0",],
+    install_requires=[
+        "requests>=2.0.0,<3.0.0",
+        "typing_extensions"
+    ],
 )
