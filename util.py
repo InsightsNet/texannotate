5c75f959ae1ac2648ea709a099d94b0ec4a202f1
Fireblossom <changxu.duan@tu-darmstadt.de>
2023-08-03 16:34:46 +0200

    explicitly specify the pdf output

--- util.py
+++ util.py
@@ -1,7 +1,8 @@
 import os
 import socket
 from contextlib import closing
-
+import re
+from pathlib import Path
 
 def find_free_port() -> int:  #https://stackoverflow.com/questions/1365265/on-localhost-how-do-i-pick-a-free-port-number
     with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
@@ -21,3 +22,32 @@ def find_latex_file(filename, basepath) -> str:
 
     #logger.debug("Reading input file %r", fnfull)
     return fullpath
+
+
+BLOCK_1 = r"""\pdfoutput=1
+\interactionmode=1
+"""
+BLOCK_2 = r"""
+\usepackage{xcolor}
+\usepackage{tcolorbox}
+\setlength { \fboxsep }{ 0pt } 
+\setlength { \fboxrule }{ 0pt }
+"""
+regex = r"^\\usepackage(\[\w+\])?\{\w+\}$"
+def postprocess_latex(filename):
+    with open(filename) as f:
+        file_string = f.read()
+    for match in re.finditer(regex, file_string, re.DOTALL | re.MULTILINE):
+        end = match.end()
+    file_string = BLOCK_1 + file_string[:end] + BLOCK_2 + file_string[end:]
+    with open(filename, 'w') as f:
+        f.write(file_string)
+
+def preprocess_latex(path):
+    p = Path(path)
+    for tex in list(p.glob(r'*.tex')) + list(list(p.glob(r'*.latex'))):
+        with tex.open() as f:
+            file_string = f.read()
+        file_string = BLOCK_1 + file_string
+        with tex.open('w') as f:
+            f.write(file_string)
